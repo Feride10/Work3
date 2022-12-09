9735952f57bef36c3458044a10d051a1f505bdf2
Feride_queen <faridka0@gmail.com>
2022-12-09 21:38:58 +0300

    Неработающая задача 19 3 семинара

---
+++ Work.ru/work2.ru/work3.ru/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+string = Console.ReadLine();
+int = number.Length;
+
+if (len = 5)
+{
+    if (number [0] = number [4] && number[1] = number[3])
+    {
+        Console.WriteLine($"{number} - палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"стоп: {number} - не является пятизначным");
+}
